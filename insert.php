97ad6b8c6170562207858462a5b624f72ef2ca2b
ValSchool <131674526+ValSchool@users.noreply.github.com>
2023-06-06 11:16:47 +0200

    insert.php

--- insert.php
+++ insert.php
@@ -1,81 +1,59 @@
-<?php
-$host = 'localhost:3307';
-
-$db   = 'test';
-
-$user = 'root';
-
-$pass = '';
-
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-
-try
-
-{
-
-$pdo = new PDO($dsn, $user, $pass, $options);
-
-echo "Connectie gemaakt!";
-} 
-catch (\PDOException $e) 
-{
-throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $product_naam = $_POST["product_naam"];
-
-    $prijs_per_stuk = $_POST["prijs_per_stuk"];
-    $omschrijving = $_POST["omschrijving"];
-
-    $query = 'INSERT INTO producten (product_naam, prijs_per_stuk, omschrijving) VALUES (:product_naam, :prijs_per_stuk, :omschrijving)';
-    $statement = $pdo->prepare($query);
-
-    $data = array(
-        "product_naam" => $product_naam,
-        "prijs_per_stuk" => $prijs_per_stuk,
-        "omschrijving" => $omschrijving
-    );
-
-    $result = $statement->execute($data);
-
-    if ($result) {
-        echo "Product  toegevoegd.";
-    } else {
-        echo "Kan het Product niet toevoegen, Errorcode: 404";
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <title>product</title>
+    <title>product toevoegen</title>
 </head>
 <body>
-    <h1>product Toevoegen</h1>
 
-    <form method="POST" action="insert.php">
+<?php
+// kijk of het formulier is verzonden
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // maak een verbinding met de database
+    $servername = "localhost";
+    $username = "jouw_gebruikersnaam";
+    $password = "jouw_wachtwoord";
+    $dbname = "winkel";
 
-        <label for="product_naam">productnaam:</label>
-        <input type="text" name="product_naam" required><br><br>
-        
-        <label for="prijs_per_stuk">prijs per stuk:</label>
+    // maaakt de verbinding
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-        <input type="number" name="prijs_per_stuk" required><br><br>
-        
-        <label for="omschrijving">omschrijving:</label>
-        <textarea name="omschrijving" required></textarea><br><br>
-        
-        <input type="submit" value="toevoegen">
-    </form>
+    // kijkt of de verbinding succesvol is
+    if ($conn->connect_error) {
+        die("Verbinding mislukt: " . $conn->connect_error);
+    }
+
+    // ontvang de gegevens uit het formulier
+    $product_naam = $_POST['product_naam'];
+    $prijs_per_stuk = $_POST['prijs_per_stuk'];
+    $omschrijving = $_POST['omschrijving'];
+
+    $sql = "INSERT INTO producten (product_naam, prijs_per_stuk, omschrijving) VALUES ('$product_naam', '$prijs_per_stuk', '$omschrijving')";
+
+    // Voer de query uit
+    if ($conn->query($sql) === TRUE) {
+        echo "product succesvol toegevoegd.<br>";
+    } else {
+        echo "fout bij het toevoegen van het product: " . $conn->error . "<br>";
+    }
+
+    // sluit de verbinding met de database af
+    $conn->close();
+}
+?>
+<h2>product toevoegen</h2>
+
+<form method="POST" action="insert.php">
+    <label for="product_naam">Product Naam:</label><br>
+    <input type="text" id="product_naam" name="product_naam" required><br><br>
+
+    <label for="prijs_per_stuk">Prijs per Stuk:</label><br>
+    <input type="text" id="prijs_per_stuk" name="prijs_per_stuk" required><br><br>
+
+    <label for="omschrijving">Omschrijving:</label><br>
+    <textarea id="omschrijving" name="omschrijving" required></textarea><br><br>
+
+    <input type="submit" value="product Toevoegen">
+</form>
+
 </body>
 </html>
